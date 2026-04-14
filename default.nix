bed5bd2db08c5828127c8eb726a6c0a07e486d78
Marius Niveri <marius.niveri@gmail.com>
2026-04-14 21:11:14 +0200

    fix-golazo

--- default.nix
+++ default.nix
@@ -1,8 +1,16 @@
 {pkgs ? import <nixpkgs> {}, ...}:
+let
+  version = "0.23.0";
+in
 pkgs.buildGoModule {
   pname = "golazo";
-  version = "0.21.0";
+  inherit version;
   vendorHash = "sha256-M2gfqU5rOfuiVSZnH/Dr8OVmDhyU2jYkgW7RuIUTd+E=";
+
+  ldflags = [
+    "-s" "-w"
+    "-X github.com/0xjuanma/golazo/cmd.Version=v${version}"
+  ];
 
   subPackages = ["."];
 
